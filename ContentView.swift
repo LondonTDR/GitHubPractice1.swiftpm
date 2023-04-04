a2f3ba11ba03ef2345772c605f072a16033b7f8a
London <londonsmith@stu.d214.org>
2023-04-04 14:55:12 -0500

    butt

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var hi :Int = 0
-    @State var hello :Int = 0
+    @State var hi = 0
+    @State var hello = 0
     var body: some View {
-        TextField("receiver", text: $hi, format: .number)
-        TextField("receiver2", text: $hello, format: .number)
+        TextField("receiver", value: $hi, format: .number)
+        TextField("receiver2", value: $hello, format: .number)
     }
 }
